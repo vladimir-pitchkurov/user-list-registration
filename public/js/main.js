85022ae017d06b06bec3d8dae2b3b505cf374cb9
vladimir-pitchkurov <vladimir.pitchkurov@gmail.com>
2018-08-24 21:15:33 +0300

    move js to main file

--- public/js/main.js
+++ public/js/main.js
@@ -1,9 +1,29 @@
 $(document).ready(function () {
+    if ($('.registration-window').length > 0) {
+        $('.registration-window').on('click', '.tab', function () {
+            $('.registration-window').find('.active').removeClass('active');
+            $(this).addClass('active');
+            $('.tab-form').eq($(this).index()).addClass('active')
+        });
+    }
 
-    $('.registration-window').on('click', '.tab', function () {
-        $('.registration-window').find('.active').removeClass('active');
-        $(this).addClass('active');
-        $('.tab-form').eq($(this).index()).addClass('active')
-    })
-
+    if ($('#exampleModal').length > 0) {
+        $('.avatar-list').click(function () {
+            var imageSrc = this.src;
+            $('#exampleModal').find('.modal-avatar-image').attr('src', imageSrc);
+            $('#exampleModal').modal();
+        });
+    }
 });
+
+function readURL(input) {
+    if (input.files && input.files[0]) {
+        var reader = new FileReader();
+
+        reader.onload = function (e) {
+            $('#blah').attr('src', e.target.result);
+        };
+
+        reader.readAsDataURL(input.files[0]);
+    }
+}
