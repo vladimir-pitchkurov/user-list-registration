2ccd90c537a9381bcd799ddd59e4b86185028f6d
vladimir-pitchkurov <vladimir.pitchkurov@gmail.com>
2018-08-24 17:49:28 +0300

    add routes

--- config/routes.php
+++ config/routes.php
@@ -1,53 +1,16 @@
 <?php
 
 return array(
-    'auth'            => 'site/index',
-    'users'            => 'site/list',
-    'user/register'  => 'user/register',
-    'user/login'    => 'user/login',
-    'user/logout'   => 'user/logout',
-    'index.php'      => 'site/index', // actionIndex в SiteController
+    'auth'                => 'site/index',
+    'users/page-([0-9]+)' => 'site/list/$1',
+    'users'               => 'site/list',
+    'user/register'       => 'user/register',
+    'user/login'          => 'user/login',
+    'user/logout'         => 'user/logout',
+    'user/edit'           => 'user/edit',
+    'user/update-image'   => 'user/image',
+    'user/update'         => 'user/update',
+    'index.php'           => 'site/index', // actionIndex в SiteController
+    ''                    => 'site/index', // actionIndex в SiteController
 
-    '' => 'site/index', // actionIndex в SiteController
-
-
-
-    // Товар:
-    'product/([0-9]+)' => 'product/view/$1', // actionView в ProductController
-    // Каталог:
-    'catalog' => 'catalog/index', // actionIndex в CatalogController
-    // Категория товаров:
-    'category/([0-9]+)/page-([0-9]+)' => 'catalog/category/$1/$2', // actionCategory в CatalogController
-    'category/([0-9]+)' => 'catalog/category/$1', // actionCategory в CatalogController
-    // Корзина:
-    'cart/checkout' => 'cart/checkout', // actionAdd в CartController
-    'cart/delete/([0-9]+)' => 'cart/delete/$1', // actionDelete в CartController
-    'cart/add/([0-9]+)' => 'cart/add/$1', // actionAdd в CartController
-    'cart/addAjax/([0-9]+)' => 'cart/addAjax/$1', // actionAddAjax в CartController
-    'cart' => 'cart/index', // actionIndex в CartController
-    // Пользователь:
-
-    'cabinet/edit' => 'cabinet/edit',
-    'cabinet' => 'cabinet/index',
-    // Управление товарами:
-    'admin/product/create' => 'adminProduct/create',
-    'admin/product/update/([0-9]+)' => 'adminProduct/update/$1',
-    'admin/product/delete/([0-9]+)' => 'adminProduct/delete/$1',
-    'admin/product' => 'adminProduct/index',
-    // Управление категориями:
-    'admin/category/create' => 'adminCategory/create',
-    'admin/category/update/([0-9]+)' => 'adminCategory/update/$1',
-    'admin/category/delete/([0-9]+)' => 'adminCategory/delete/$1',
-    'admin/category' => 'adminCategory/index',
-    // Управление заказами:
-    'admin/order/update/([0-9]+)' => 'adminOrder/update/$1',
-    'admin/order/delete/([0-9]+)' => 'adminOrder/delete/$1',
-    'admin/order/view/([0-9]+)' => 'adminOrder/view/$1',
-    'admin/order' => 'adminOrder/index',
-    // Админпанель:
-    'admin' => 'admin/index',
-    // О магазине
-    'contacts' => 'site/contact',
-    'about' => 'site/about',
-    // Главная страница
 );
