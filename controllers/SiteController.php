9f2e753860d65aab6276f301d9f753f9b29a7e07
vladimir-pitchkurov <vladimir.pitchkurov@gmail.com>
2018-08-24 21:16:10 +0300

    simple guard

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -21,17 +21,12 @@ class SiteController
 
     public function actionList($page = 1)
     {
-        if (Auth::isGuest() || !Auth::userId()) {
-            header("Location: /auth");
-            return true;
-        }
+        Auth::guardedRoute();
 
         $empty_image = '/public/uploads/images/empty.jpg';
         $users = User::getAllForPage($page);
         $total = intval(User::countUsers()[0]);
-
         $pagination = new Pagination($total, $page, User::SHOW_BY_DEFAULT, 'page-');
-
 
         include_once(ROOT . '/public/views/user-list.php');
         return true;
