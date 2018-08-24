899a74f4e05c77e7816c90aed189b4070cf7cdd2
vladimir-pitchkurov <vladimir.pitchkurov@gmail.com>
2018-08-24 17:46:56 +0300

    register, edit, count, all, image

--- models/User.php
+++ models/User.php
@@ -1,51 +1,72 @@
 <?php
 
 
-
 class User
 {
+    const SHOW_BY_DEFAULT = 7;
 
     public static function register($user)
     {
-        $db = Db::getConnection();
-
         $sql = 'INSERT INTO user (first_name, last_name, email, password, description) '
-                . 'VALUES (:first_name, :last_name, :email, :password, :description)';
+            . 'VALUES (:first_name, :last_name, :email, :password, :description)';
         $hash_password = md5($user['password']);
 
-        $result = $db->prepare($sql);
+        $result = Db::getConnection()->prepare($sql);
         $result->bindParam(':first_name', $user['first_name'], PDO::PARAM_STR);
         $result->bindParam(':last_name', $user['last_name'], PDO::PARAM_STR);
         $result->bindParam(':email', $user['email'], PDO::PARAM_STR);
         $result->bindParam(':password', $hash_password, PDO::PARAM_STR);
         $result->bindParam(':description', $user['description'], PDO::PARAM_STR);
+        if ($result->execute()) {
+            return Db::getConnection()->lastInsertId();;
+        } else {
+            $errors[] = "Ошибка регистрации.";
+            return false;
+        }
+    }
 
+    public static function edit($data)
+    {
+        $id = Auth::userId();
+        $sql = "UPDATE user 
+            SET first_name = :first_name, last_name = :last_name, description = :description 
+            WHERE id = :id";
+
+        $result = Db::getConnection()->prepare($sql);
+        $result->bindParam(':id', $id, PDO::PARAM_INT);
+        $result->bindParam(':first_name', $data['first_name'], PDO::PARAM_STR);
+        $result->bindParam(':last_name', $data['last_name'], PDO::PARAM_STR);
+        $result->bindParam(':description', $data['description'], PDO::PARAM_STR);
         return $result->execute();
     }
 
-    public static function edit($id, $name, $password)
+    public static function logout()
     {
-        $db = Db::getConnection();
-
+        $id = Auth::userId();
+        $id = intval($id);
+        $empty = 'empty';
         $sql = "UPDATE user 
-            SET name = :name, password = :password 
+            SET session_token = :session_token
             WHERE id = :id";
 
-        $result = $db->prepare($sql);
+        $result = Db::getConnection()->prepare($sql);
         $result->bindParam(':id', $id, PDO::PARAM_INT);
-        $result->bindParam(':name', $name, PDO::PARAM_STR);
-        $result->bindParam(':password', $password, PDO::PARAM_STR);
+        $result->bindParam(':session_token', $empty, PDO::PARAM_STR);
         return $result->execute();
+    }
+
+    public static function countUsers()
+    {
+        return Db::getConnection()->query('SELECT count(id) FROM user')->fetch();
     }
 
     public static function checkUserData($email, $password)
     {
-        $db = Db::getConnection();
         $password = md5($password);
 
         $sql = 'SELECT * FROM user WHERE email = :email AND password = :password';
 
-        $result = $db->prepare($sql);
+        $result = Db::getConnection()->prepare($sql);
         $result->bindParam(':email', $email, PDO::PARAM_STR);
         $result->bindParam(':password', $password, PDO::PARAM_STR);
         $result->execute();
@@ -58,40 +79,9 @@ class User
         return false;
     }
 
-    public static function auth($userId)
-    {
-        $_SESSION['user'] = $userId;
-    }
-
-    public static function checkLogged()
-    {
-        // Если сессия есть, вернем идентификатор пользователя
-        if (isset($_SESSION['user'])) {
-            return $_SESSION['user'];
-        }
-
-        header("Location: /user/login");
-    }
-
-    public static function isGuest()
-    {
-        if (isset($_SESSION['user'])) {
-            return false;
-        }
-        return true;
-    }
-
     public static function checkName($name)
     {
         if (strlen($name) >= 2) {
-            return true;
-        }
-        return false;
-    }
-
-    public static function checkPhone($phone)
-    {
-        if (strlen($phone) >= 10) {
             return true;
         }
         return false;
@@ -105,21 +95,11 @@ class User
         return false;
     }
 
-    public static function checkEmail($email)
-    {
-        if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
-            return true;
-        }
-        return false;
-    }
-
     public static function checkEmailExists($email)
     {
-        $db = Db::getConnection();
-
         $sql = 'SELECT COUNT(*) FROM user WHERE email = :email';
 
-        $result = $db->prepare($sql);
+        $result = Db::getConnection()->prepare($sql);
         $result->bindParam(':email', $email, PDO::PARAM_STR);
         $result->execute();
 
@@ -130,13 +110,10 @@ class User
 
     public static function getUserById($id)
     {
-        $db = Db::getConnection();
-
         $sql = 'SELECT * FROM user WHERE id = :id';
 
-        $result = $db->prepare($sql);
+        $result = Db::getConnection()->prepare($sql);
         $result->bindParam(':id', $id, PDO::PARAM_INT);
-
         $result->setFetchMode(PDO::FETCH_ASSOC);
         $result->execute();
 
@@ -146,11 +123,9 @@ class User
     public static function setToken($id, $token)
     {
         $login_time = time();
-        $db = Db::getConnection();
         $sql = 'UPDATE user SET session_token = :token , last_login = :last_login WHERE id = :uid';
 
-        //var_dump("id: ", $id, 'tiken:' , $token);die;
-        $result = $db->prepare($sql);
+        $result = Db::getConnection()->prepare($sql);
 
         $result->bindParam(':last_login', $login_time, PDO::PARAM_INT);
         $result->bindParam(':token', $token, PDO::PARAM_STR);
@@ -158,14 +133,41 @@ class User
         return $result->execute();
     }
 
+    public static function setAvatar($url)
+    {
+        $id = Auth::userId();
+        $sql = 'UPDATE user SET image = :image WHERE id = :uid';
+
+        $result = Db::getConnection()->prepare($sql);
+
+        $result->bindParam(':image', $url, PDO::PARAM_STR);
+        $result->bindParam(':uid', $id, PDO::PARAM_INT);
+        return $result->execute();
+    }
+
     public static function removeToken($id)
     {
-        $db = Db::getConnection();
-        $sql = 'UPDATE user SET session_token = NULL WHERE id = :uid';
-        $result = $db->prepare($sql);
-        $result->bindParam(':id', $id, PDO::PARAM_INT);
+        $empty = 'empty';
+        $intId = intval($id);
+        $sql = 'UPDATE user SET session_token = :empty WHERE id = :uid';
+        $result = Db::getConnection()->prepare($sql);
+        $result->bindParam(':uid', $intId, PDO::PARAM_INT);
+        $result->bindParam(':empty', $empty, PDO::PARAM_STR);
 
         return $result->execute();
     }
 
+    public static function getAllForPage($page)
+    {
+        $limit = self::SHOW_BY_DEFAULT;
+        $offset = ($page - 1) * $limit;
+        $sql = 'SELECT id, first_name, last_name, email, image, description FROM user '
+            . 'ORDER BY id ASC LIMIT :limit OFFSET :offset';
+
+        $prepared = Db::getConnection()->prepare($sql);
+        $prepared->bindParam(':limit', $limit, PDO::PARAM_INT);
+        $prepared->bindParam(':offset', $offset, PDO::PARAM_INT);
+        $prepared->execute();
+        return $prepared->fetchAll();
+    }
 }
