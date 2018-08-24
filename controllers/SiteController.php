fdb6db6e1772688f85279504833297720759e9c1
vladimir-pitchkurov <vladimir.pitchkurov@gmail.com>
2018-08-24 17:51:01 +0300

    user list routing

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -11,14 +11,29 @@ class SiteController
 
     public function actionIndex()
     {
-        include_once ROOT.'/public/views/login-page.php';
+        if (!Auth::isGuest() || Auth::userId()) {
+            header("Location: /users");
+            return true;
+        }
+        include_once ROOT . '/public/views/login-page.php';
         return true;
     }
 
-    public function actionList()
+    public function actionList($page = 1)
     {
-        var_dump($_SESSION);
-        echo 'Hello from List!';
+        if (Auth::isGuest() || !Auth::userId()) {
+            header("Location: /auth");
+            return true;
+        }
+
+        $empty_image = '/public/uploads/images/empty.jpg';
+        $users = User::getAllForPage($page);
+        $total = intval(User::countUsers()[0]);
+
+        $pagination = new Pagination($total, $page, User::SHOW_BY_DEFAULT, 'page-');
+
+
+        include_once(ROOT . '/public/views/user-list.php');
         return true;
     }
 
