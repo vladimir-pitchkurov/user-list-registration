47cd2b1c3a88e4a8bb5862982af25ae94c39d637
vladimir-pitchkurov <vladimir.pitchkurov@gmail.com>
2018-08-24 21:14:36 +0300

    add bootstrap-md inputs

--- public/views/login-page.php
+++ public/views/login-page.php
@@ -1,10 +1,10 @@
-<?php
-include ROOT . '/public/views/layouts/header.php';
+<?php include ROOT . '/public/views/layouts/header.php';
 if (isset($errors)):
     foreach ($errors as $error):?>
         <p><?= $error; ?></p>
     <? endforeach;
 endif; ?>
+
     <div class="registration-wrapper ">
 
         <div class="registration-window z-depth-3">
@@ -14,72 +14,55 @@ endif; ?>
             <form method="post" action="/user/register" class="text-center border border-light active tab-form p-5">
 
                 <div class="form-row mb-4">
-                    <div class="col">
-                        <!-- First name -->
-                        <input type="text" name="first_name" class="form-control" placeholder="First name" required>
+                    <div class="col md-form">
+                        <input type="text" name="first_name" id="reg-fn" class="form-control" required>
+                        <label for="reg-fn">First Name</label>
+
                     </div>
-                    <div class="col">
-                        <!-- Last name -->
-                        <input type="text" name="last_name" class="form-control" placeholder="Last name" required>
+                    <div class="col md-form">
+                        <input type="text" name="last_name" id="reg-ln" class="form-control" required>
+                        <label for="reg-ln">Last Name</label>
                     </div>
                 </div>
 
-                <!-- E-mail -->
-                <input type="email" name="email" class="form-control mb-4" placeholder="E-mail" required>
+                <div class="md-form">
+                    <input type="email" id="rg-mail" name="email" class="form-control mb-4" required>
+                    <label for="rg-mail">E-mail</label>
+                </div>
+                <div class="md-form">
+                    <input type="password" id="rg-pass" name="password" class="form-control" required>
+                    <label for="rg-pass">Password</label>
+                    <small class="form-text text-muted mb-4">At least 6 characters or digit</small>
+                </div>
+                <div class="md-form">
+                    <textarea name="description" id="rg-description" class="description" required></textarea>
+                    <label for="rg-description">Description</label>
+                </div>
 
-                <!-- Password -->
-                <input type="password" name="password" class="form-control" placeholder="Password" required>
-                <small class="form-text text-muted mb-4">
-                    At least 6 characters or digit
-                </small>
-
-
-                <textarea name="description" class="description" placeholder="description" required></textarea>
-
-
-                <!-- Sign up button -->
-                <button class="btn btn-info my-4 btn-block" type="submit">Sign in</button>
-
+                <button class="btn btn-info my-4 btn-block" type="submit">Register</button>
 
             </form>
-            <!-- Default form register -->
-
 
             <form method="post" action="/user/login" name="sign-in"
                   class="text-center border border-light tab-form p-5 del">
 
-                <!-- E-mail -->
-                <input type="email" name="login-email" class="form-control mb-4" placeholder="E-mail" required>
+                <div class="md-form">
+                    <input type="email" id="login-email" name="login-email" class="form-control mb-4" required>
+                    <label for="login-email">E-mail</label>
+                </div>
+                <div class="md-form">
+                    <input type="password" id="login-password" name="login-password" class="form-control" required>
+                    <label for="login-password">Password</label>
+                    <small class="form-text text-muted mb-4">
+                        At least 6 characters or digit
+                    </small>
+                </div>
 
-                <!-- Password -->
-                <input type="password" name="login-password" class="form-control" placeholder="Password" required>
-                <small class="form-text text-muted mb-4">
-                    At least 6 characters or digit
-                </small>
-
-                <!-- Sign up button -->
                 <button class="btn btn-info my-4 btn-block" type="submit">Sign in</button>
 
-
             </form>
-            <!-- Default form sign in -->
         </div>
-
     </div>
-
-    <script type="text/javascript">
-        function readURL(input) {
-            if (input.files && input.files[0]) {
-                var reader = new FileReader();
-
-                reader.onload = function (e) {
-                    $('#blah').attr('src', e.target.result);
-                }
-
-                reader.readAsDataURL(input.files[0]);
-            }
-        }
-    </script>
 
 <?php
 include ROOT . '/public/views/layouts/footer.php'; ?>
