e1bbf7b3ba70f946a46ee5ec770ac1ab7cf77d8a
vladimir-pitchkurov <vladimir.pitchkurov@gmail.com>
2018-08-24 17:51:49 +0300

    clear codestile

--- index.php
+++ index.php
@@ -6,15 +6,12 @@ error_reporting(E_ALL);
 define('ROOT', dirname(__FILE__));
 require_once(ROOT.'/components/Autoload.php');
 $session_settings = include (ROOT.'/config/session_time.php');
-
+$now = time();
 
 session_start();
 
-$now = time();
 if (isset($_SESSION['discard_after']) && $now > $_SESSION['discard_after']) {
-    session_unset();
-    session_destroy();
-    session_start();
+    Auth::clearSession();
 }
 
 $_SESSION['discard_after'] = $now + $session_settings['session_live_time'];
