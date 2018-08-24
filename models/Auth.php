b2d0ed997be2ee6395aa0b9ba13e8995ab72478e
vladimir-pitchkurov <vladimir.pitchkurov@gmail.com>
2018-08-24 17:50:16 +0300

    upgrade auth

--- models/Auth.php
+++ models/Auth.php
@@ -10,21 +10,38 @@ class Auth
 {
     public static function isGuest()
     {
-
-
+        return (!isset($_SESSION['token']) && !isset($_SESSION['uid']));
     }
 
     public static function userId()
     {
+        if (self::isGuest()) {
+            return false;
+        } else {
+            $session_time = (include(ROOT . '/config/session_time.php'))['session_live_time'];
+            $user = User::getUserById(intval($_SESSION['uid']));
+            if (!isset($user)) {
+                return false;
+            }
+            return ($user['session_token'] == $_SESSION['token']
+                && intval($user['last_login']) > time() - (intval($session_time)))
+                ? $user['id'] : false;
+        }
 
     }
 
-    public static function addToSession( $user)
+    public static function addToSession($user)
     {
-        $token = md5($user['id'].$user['first_name'].time());
+        $token = md5($user['id'] . $user['first_name'] . time());
         $_SESSION['token'] = $token;
         $_SESSION['uid'] = $user['id'];
         return $token;
     }
 
+    public static function clearSession()
+    {
+        session_unset();
+        session_destroy();
+        session_start();
+    }
 }
